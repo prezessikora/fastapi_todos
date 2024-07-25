8d208f46b6b3a7b75fa72aa61462329c3df11df7
Krzysztof Sikora <krzysztofsikora@merixstudiosmbp.lan>
2024-07-25 16:35:56 +0200

    Finished users router tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -15,3 +15,19 @@ def test_return_user(test_user):
     assert response.json()['last_name'] == 'Sikora'
     assert response.json()['phone_number'] == '111-111-111'
 
+def test_change_password_success(test_user):
+    response = client.put('/users/password',json={
+        'password' : 'onyx',
+        'new_password' : 'xyzxyz',
+    })
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+def test_change_password_invalid_password(test_user):
+    response = client.put('/users/password',json={
+        'password' : 'onyx1',
+        'new_password' : 'xyzxyz',
+    })
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
